5f8aecb3607979ab7ff6a2efbc85b93fbc080ff5
박정연 <55582462+wjdduszzang95@users.noreply.github.com>
2022-04-29 15:48:01 +0900

    Update and rename Test.py to see the arguments passed.py

--- scripts/see the arguments passed.py
+++ scripts/see the arguments passed.py
@@ -1,4 +1,4 @@
-// 메소드가 호출될 때 기존의 프로그램 동작은 건들지 않고 전달되는 인자를 보고싶을 때
+// When you want to see the arguments passed without touching the existing program behavior when a method is called
 Java.perform(function() { 
   YourClass.Method.implementation = function(arg1, arg2) { // rely on the number of args.
     console.log("arg1 : " + arg1);
@@ -6,3 +6,5 @@ Java.perform(function() {
     return this.Method;
   }
 });
+
+
